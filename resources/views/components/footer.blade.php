7e03116cb714cdfc2974b96cf7d1df9ca2f38cd7
jamaalul <a.jamaalul@gmail.com>
2025-12-22 22:24:58 +0700

    Fix footer & news.show

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,41 +1,42 @@
 <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 
 <footer>
-        <div class="footer-container">
-            <div class="footer-left">
-                <div class="footer-top">
-                    <img src="{{ asset('assets/logo_kabinet.png') }}" alt="logo_kabinet">
-                    <div class="footer-top-text">
-                        <h2>HIMA D4 Teknik Informatika</h2>
-                        <p>KABINET SELARAS 2025</p>
-                    </div>
+    <div class="footer-container">
+        <div class="footer-left">
+            <div class="footer-top">
+                <img src="{{ asset('assets/logo_kabinet.png') }}" alt="logo_kabinet">
+                <div class="footer-top-text">
+                    <h2>HIMA D4 Teknik Informatika</h2>
+                    <p>KABINET SELARAS 2025</p>
                 </div>
-                <div class="footer-social">
-                    <a href="#"><img src="{{ asset('assets/youtube.svg') }}" alt="youtube"></a>
-                    <a href="#"><img src="{{ asset('assets/instagram.svg') }}" alt="instagram"></a>
-                </div>
-                <p class="copyright">©2025 HIMPUNAN MAHASISWA TEKNIK INFORMATIKA</p>
             </div>
-            <div class="footer-right">
-                <div class="footer-links">
+            <div class="footer-social">
+                <a href="#"><img src="{{ asset('assets/youtube.svg') }}" alt="youtube"></a>
+                <a href="#"><img src="{{ asset('assets/instagram.svg') }}" alt="instagram"></a>
+            </div>
+            <p class="copyright">©2025 HIMPUNAN MAHASISWA TEKNIK INFORMATIKA</p>
+        </div>
+        <div class="footer-right">
+            <div class="footer-links">
 
-                    <h3>Features</h3>
-                    <a href="#">About Us</a>
-                    <a href="#">News</a>
-                    <a href="#">Company Profile</a>
-                    <a href="#">Marketplace</a>
-                    <a href="#">SOP Partnership</a>
-                    <a href="#">SOP MEDINFO</a>
-                </div>
-                <div class="footer-links">
-                    <h3>Social Media</h3>
-                    <a href="#">Youtube</a>
-                    <a href="#">Instagram</a>
-                </div>
-                <div class="footer-links">
-                    <h3>Alamat</h3>
-                    <a href="#">Lorem Ipsum</a>
-                </div>
+                <h3>Features</h3>
+                <a href="#">About Us</a>
+                <a href="#">News</a>
+                <a href="#">Company Profile</a>
+                <a href="#">Marketplace</a>
+                <a href="#">SOP Partnership</a>
+                <a href="#">SOP MEDINFO</a>
+            </div>
+            <div class="footer-links">
+                <h3>Social Media</h3>
+                <a href="#">Youtube</a>
+                <a href="#">Instagram</a>
+            </div>
+            <div class="footer-links">
+                <h3>Alamat</h3>
+                <a href="#">Jl. Dharmawangsa Dalam Selatan No.28 – 30, Airlangga, Kec. Gubeng, Surabaya, Jawa
+                    Timur 60286</a>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,51 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-   <link rel="stylesheet" href="{{ asset('css/news/show.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/news/show.css') }}">
     <section class="hero">
-        <div class="overlay" style="background-image: url('{{ asset('images/news/medinfo.png') }}');">
+        <div class="overlay" style="background-image: url('{{ asset('storage/' . $news->featured_image) }}') }}');">
             <div class="overlay-content">
                 <h1>{{ $news->title }}</h1>
                 <p>{{ $news->author->name }}</p>
                 <p>{{ \Carbon\Carbon::parse($news->published_at)->translatedFormat('j F Y') }}</p>
             </div>
         </div>
-   </section>
-   <section class="content">
-      <div class="info" data-aos="fade-up" data-aos-duration="1000">
-         <h1>{{ $news['title'] }}</h1>
-         <p class="date">{{ $news['published_at'] }}</p>
-         <div class="body">
-            @if($news->featured_image)
-            <div class="mb-8 image-wrapper">
-                <img src="{{ asset('storage/' . $news->featured_image) }}" 
-                    alt="{{ $news->title }}" 
-                    class="news-image">
+    </section>
+    <section class="content">
+        <div class="info" data-aos="fade-up" data-aos-duration="1000">
+            <h1>{{ $news['title'] }}</h1>
+            <p class="date">{{ $news['published_at'] }}</p>
+            <div class="body">
+                @if ($news->featured_image)
+                    <div class="mb-8 image-wrapper">
+                        <img src="{{ asset('storage/' . $news->featured_image) }}" alt="{{ $news->title }}"
+                            class="news-image">
+                    </div>
+                    <br>
+                @endif
+                <div class="text-gray-800 leading-relaxed">
+                    {!! nl2br(e($news->content)) !!}
+                </div>
             </div>
-            <br>
-            @endif
-            <div class="text-gray-800 leading-relaxed">
-                {!! nl2br(e($news->content)) !!}
+        </div>
+        <div class="additional">
+            <h2 data-aos="zoom-in-down" data-aos-duration="1000">INFORMASI LAINNYA</h2>
+            <div class="other-info">
+                @foreach ($relatedNews as $related)
+                    <a href="{{ route('news.show', $related) }}">
+
+                        <div class="info-card" data-aos="fade-up" data-aos-duration="1000">
+                            <div class="top">
+                                <h3>{{ $related['title'] }}</h3>
+                            </div>
+                            <div class="bottom">
+                                {{-- <p>{!! \Illuminate\Support\Str::limit(strip_tags($related['excerpt']), 40, '...') !!}</p> --}}
+                                <p class="date">{{ $related['published_at']->translatedFormat('j F Y') }}</p>
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
             </div>
-         </div>
-      </div>
-      <div class="additional">
-         <h2 data-aos="zoom-in-down" data-aos-duration="1000">INFORMASI LAINNYA</h2>
-         <div class="other-info">
-            @foreach ($relatedNews as $related)
-                <a href="{{ route('news.show', $related) }}">
-                    
-               <div class="info-card" data-aos="fade-up" data-aos-duration="1000">
-                  <div class="top">
-                     <h3>{{ $related['title'] }}</h3></div>
-                  <div class="bottom">
-                     {{-- <p>{!! \Illuminate\Support\Str::limit(strip_tags($related['excerpt']), 40, '...') !!}</p> --}}
-                     <p class="date">{{ $related['published_at']->translatedFormat('j F Y') }}</p>
-                  </div>
-               </div>
-               </a>
-            @endforeach
-         </div>
-      </div>
-   </section>
+        </div>
+    </section>
 @endsection
